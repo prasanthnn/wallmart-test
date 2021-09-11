8a09faf00ffb22f89edda96a973eddf5cfce42ae
Sibani M <mailtosibani27@gmail.com>
2021-09-11 21:12:46 +0530

    updated in dev branch

--- DButils.java
+++ DButils.java
@@ -1,7 +1,5 @@
 updating the file
-Hello Guys This is the JAVA file .
-<<<<<<< HEAD
+Hello Guys This is the JAVA file 
 Second updation at 8:53 pm.
 
-=======
->>>>>>> parent of 73250ec (added test files)
+I am developing this line in dev branch. 
